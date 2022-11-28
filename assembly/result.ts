c278bf0773c37aed421c92de78a7bbd18785ed30
Grégory Libert <greg_libert@yahoo.fr>
2022-11-28 23:45:12 +0100

    fixes format

---
+++ assembly/result.ts
@@ -0,0 +1,41 @@
+/**
+ * Create a Result object.
+ * A result can be:
+ * - expected Type if the treatment was executed as planed.
+ * - an error if something went wrong.
+ */
+export class Result<T> {
+  constructor(
+    // expected value for passing case
+    public value: T,
+    // error message for non-passing case
+    public error: string | null = null,
+  ) {}
+
+  /**
+   * Checks that the result is okay.
+   * @return {boolean}
+   */
+  isOk(): bool {
+    return !this.error;
+  }
+
+  /**
+   * Checks that the result is okay and panic if not.
+   *
+   * @param {string} msg - panic message that will prefix the error content.
+   * @return {T}
+   */
+  expect(msg: string | null = null): NonNullable<T> {
+    if (!this.isOk()) {
+      assert(false, msg ? `${msg}: ${this.error!}` : `${this.error!}`);
+    }
+
+    if (isNullable<T>()) {
+      return this.value!;
+    } else {
+      // @ts-ignore: not nullable
+      return this.value;
+    }
+  }
+}
