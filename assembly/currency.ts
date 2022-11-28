18cf2a877d5f4c52eb48f6334dec976a1b9908d3
Grégory Libert <greg_libert@yahoo.fr>
2022-11-28 23:46:46 +0100

    updates currency with argument and result

--- assembly/currency.ts
+++ assembly/currency.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
-import {Valider} from './valider';
-import {ByteArray} from './byteArray';
+import {Args} from './argument';
+import {Result} from './result';
 
 /**
  * Monetary unit used to express a value.
@@ -16,150 +16,54 @@ import {ByteArray} from './byteArray';
  *
  * const dollar = new Currency("dollar", 2);
  */
-export class Currency implements Valider {
-  _isValid: bool;
-  _minorUnit: u8;
-  _name: string;
-
+export class Currency {
   /**
    * Creates a new instance of Currency.
    *
    * @param {string} name - name of the currency.
-   * @param {u8} unit - minor unit of the currency.
-   * @param {bool} isValid - is the instanciate object a valid currency ?
+   * @param {u8} minorUnit - minor unit of the currency.
    */
-  constructor(name: string = '', unit: u8 = 0, isValid: bool = true) {
-    this._minorUnit = unit;
-    this._name = name;
-    this._isValid = isValid;
-  }
+  constructor(public name: string = '', public minorUnit: u8 = 0) {}
 
   /**
-   * Returns the size of the fractional part.
+   * Creates a Result Currency from given argument
    *
-   * @return {u8} Size in number of digits
+   * @param {Args} args
+   * @return {Result<Currency>}
    */
-  minorUnit(): u8 {
-    return this._minorUnit;
-  }
-
-  /**
-   * Returns the name the currency.
-   *
-   * @return {string} Currency name.
-   */
-  name(): string {
-    return this._name;
-  }
-
-  /**
-   * Returns an invalid currency
-   *
-   * @return {Currency}
-   */
-  static invalid(): Currency {
-    return new Currency('', 0, false);
-  }
-
-  /**
-   * Returns if the Amount is still valid.
-   * @return {bool}
-   */
-  isValid(): bool {
-    return this._isValid;
-  }
-
-  /**
-   * Returns the offset of the next element after having parsed an address from a string segment.
-   *
-   * The string segment can contains more thant on serialized element.
-   *
-   * @param {string} bs
-   * @param {i32} begin
-   * @return {i32}
-   */
-  fromStringSegment(bs: string, begin: i32 = 0): i32 {
-    const length = u8(bs.codePointAt(begin));
-    const c = Currency.fromByteString(bs.slice(begin + 1, begin + length + 1));
-    this._name = c._name;
-    this._minorUnit = c._minorUnit;
-    this._isValid = c._isValid;
-    return begin + length + 1;
-  }
-
-  /**
-   * Returns a string segment.
-   *
-   * The string segment can be concatenated with others to serialize multiple elements.
-   *
-   * @return {string}
-   */
-  toStringSegment(): string {
-    const bs = this.toByteString();
-    return String.fromCharCode(u8(bs.length)).concat(bs);
-  }
-
-  /**
-   * Returns a Currency from a byte string.
-   *
-   * Format is:
-   * - 1 byte for minor unit
-   * - variable for name
-   *
-   *
-   * @param {string} bs - Byte string
-   *
-   * @return {Currency}
-   */
-  static fromByteString(bs: string): Currency {
-    const a = ByteArray.fromByteString(bs);
-
-    return this.fromByteArray(a);
-  }
-
-  /**
-   * Serializes to byte string.
-   * @return {string}
-   */
-  toByteString(): string {
-    return this.toByteArray().toByteString();
-  }
-
-  /**
-   * Returns a Currency from an Uint8Array.
-   *
-   * Format is:
-   * - 1 byte for minor unit
-   * - variable for name
-   *
-   *
-   * @param {string} a
-   *
-   * @return {Currency}
-   */
-  static fromByteArray(a: Uint8Array): Currency {
-    if (a.length < 2) {
-      return Currency.invalid();
+  static fromArgs(args: Args): Result<Currency> {
+    const minorUnit = args.nextU8();
+    if (!minorUnit.isOk()) {
+      return new Result(new Currency(), minorUnit.error);
     }
 
-    const minorUnit = a[0];
-    const name = ByteArray.fromUint8Array(a.subarray(1)).toByteString();
+    const name = args.nextString();
+    if (!name.isOk()) {
+      return new Result(new Currency(), name.error);
+    }
 
-    return new Currency(name, minorUnit);
+    return new Result(new Currency(name.value, minorUnit.value));
   }
 
   /**
-   * Serializes to ByteArray.
-   * @return {ByteArray}
+   * Updates Args with current currency serialized.
    */
-  toByteArray(): ByteArray {
-    if (!this.isValid) {
-      return new ByteArray(0);
-    }
+  addArgs(args: Args): void {
+    args.add(this.minorUnit);
+    args.add(this.name);
+  }
 
-    const ba = ByteArray.fromU8(this._minorUnit);
+  /**
+   * Returns a new Args containing current currency serialized.
+   *
+   * @return {Args}
+   */
+  toArgs(): Args {
+    const args = new Args();
 
-    return ba.concat(ByteArray.fromByteString(this._name));
+    this.addArgs(args);
+
+    return args;
   }
 
   /**
@@ -170,11 +74,11 @@ export class Currency implements Valider {
    */
   @operator('==')
   equals(other: Currency): boolean {
-    if (!this._isValid || !other.isValid()) {
+    /* if (!this._isValid || !other.isValid()) {
       return false;
-    }
+    }*/
 
-    return this._minorUnit == other.minorUnit() && this._name == other.name();
+    return this.minorUnit == other.minorUnit && this.name == other.name;
   }
 
   /**
