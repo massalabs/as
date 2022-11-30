27b4c86c47b0ea7cf6948346b995892de0da09de
Nathan Seva <thykof@protonmail.ch>
2022-11-30 15:41:44 +0100

    cd: publish the 3 packages

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -17,13 +17,7 @@ case $PACKAGE in
         fi
         # remove husky install script
         npm pkg delete scripts.prepare
-        npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
-        #Use timestamp as package suffix
-        TIME=$(date -u +%Y%m%d%H%M%S)
-        sed -i "/version/s/dev.0/dev.$TIME/g" package.json
-        PUBLISH_VERSION=$(cat package.json | jq -r '.version')
-        echo publishing $NPM_PACKAGE@$PUBLISH_VERSION
-        npm publish --access public --tag dev
+        npm publish --access public
         ;;
     *)
         echo "Unknown package $NPM_PACKAGE"
