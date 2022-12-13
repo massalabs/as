5b0745236af23ebc2d8d805106d91e3641cbb758
Nathan Seva <thykof@protonmail.ch>
2022-12-13 10:05:12 +0100

    adds RangeTransform.visit (#54)

--- transformer/src/transformers/file2ByteArray.ts
+++ transformer/src/transformers/file2ByteArray.ts
@@ -4,6 +4,7 @@ import {
   StringLiteralExpression,
 } from 'assemblyscript/dist/assemblyscript.js';
 import {SimpleParser} from 'visitor-as';
+import {RangeTransform} from 'visitor-as/dist/transformRange.js';
 
 import * as fs from 'fs';
 
@@ -15,6 +16,7 @@ export class File2ByteArray {
     const bytes = JSON.stringify(fs.readFileSync(arg0.value).toJSON().data);
     let res = SimpleParser.parseExpression(bytes);
     res.range = node.range; // same range
-    return res; // replace node
+    // We need RangeTransform here to keep the attributes of the original node only updating its content
+    return RangeTransform.visit(res, node); // replace node
   }
 }
