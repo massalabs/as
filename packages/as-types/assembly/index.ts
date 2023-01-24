94ef86b39dd2b5af5f8c50bf6559750ecb214de0
Nathan Seva <thykof@protonmail.ch>
2023-01-24 17:09:53 +0000

    remove ByteArray, add Serializable
    Update packages/as-types/assembly/serializable.ts
    
    Co-authored-by: Grégory Libert <greg_libert@yahoo.fr>

--- packages/as-types/assembly/index.ts
+++ packages/as-types/assembly/index.ts
@@ -1,8 +1,8 @@
 // The entry file of your WebAssembly module.
 export * from './amount';
 export * from './argument';
-export * from './byteArray';
 export * from './currency';
 export * from './result';
 export * from './valider';
 export * from './serialization';
+export * from './serializable';
---
+++ packages/as-types/assembly/serializable.ts
@@ -0,0 +1,15 @@
+/**
+ * Implement this interface so that you can use `Args` to serialize/de-serialize your object
+ */
+export interface Serializable {
+  /**
+   * @returns the bytes
+   */
+  serialize(): StaticArray<u8>;
+  /**
+   * @param data - the serialized data
+   * @param offset - current offset
+   * @returns the length of the deserialized object
+   */
+  deserialize(data: StaticArray<u8>, offset: i32): i32;
+}
