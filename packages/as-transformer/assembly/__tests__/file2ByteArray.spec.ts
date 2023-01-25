41bae317caf117cb2324db7a852d60e6fdb6be2a
Nathan Seva <thykof@protonmail.ch>
2023-01-25 10:30:06 +0000

    install as-types in transformer

--- packages/as-transformer/assembly/__tests__/file2ByteArray.spec.ts
+++ packages/as-transformer/assembly/__tests__/file2ByteArray.spec.ts
@@ -1,12 +1,10 @@
-function strToStaticArray(str: string): StaticArray<u8> {
-  return changetype<StaticArray<u8>>(String.UTF8.encode(str));
-}
+import { stringToBytes } from '@massalabs/as-types';
 
 describe('File_to_Byte_Array transformer test', () => {
   test('File_to_Byte_Array', () => {
     const bytes: StaticArray<u8> = fileToByteArray(
       'packages/as-transformer/assembly/__tests__/fixtures/yolo',
     );
-    expect(bytes).toStrictEqual(strToStaticArray('YOLO\n'));
+    expect(bytes).toStrictEqual(stringToBytes('YOLO\n'));
   });
 });
