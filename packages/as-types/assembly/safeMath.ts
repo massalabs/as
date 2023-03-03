193177b167c1d04e71a2ee9aeed2ec45b24a59d5
Pierre Seznec <ps@massa.net>
2023-03-03 15:39:19 +0100

    fix JS docs

--- packages/as-types/assembly/safeMath.ts
+++ packages/as-types/assembly/safeMath.ts
@@ -1,61 +1,53 @@
 export class SafeMath {
   /**
    *
-   * @param a
-   * @param b
    * @returns Returns the addition of two unsigned integers,
    * reverting on overflow.
    */
   static add(a: u64, b: u64): u64 {
-      const c: u64 = a + b;
-      assert(c >= a, "SafeMath: addition overflow");
+    const c: u64 = a + b;
+    assert(c >= a, 'SafeMath: addition overflow');
 
-      return c;
+    return c;
   }
 
   /**
    *
-   * @param a
-   * @param b
    * @returns Returns the integer division of two unsigned integers. Reverts with custom message on
    * division by zero. The result is rounded towards zero.
    */
   static sub(a: u64, b: u64): u64 {
-      assert(b <= a, "SafeMath: subtraction overflow");
-      const c: u64 = a - b;
+    assert(b <= a, 'SafeMath: subtraction overflow');
+    const c: u64 = a - b;
 
-      return c;
+    return c;
   }
 
   /**
    *
-   * @param a
-   * @param b
    * @returns Returns the multiplication of two unsigned integers, reverting on
    * overflow.
    */
   static mul(a: u64, b: u64): u64 {
-      if (a == 0) {
-          return 0;
-      }
+    if (a == 0) {
+      return 0;
+    }
 
-      const c = a * b;
-      assert(c / a == b, "SafeMath: multiplication overflow");
+    const c = a * b;
+    assert(c / a == b, 'SafeMath: multiplication overflow');
 
-      return c;
+    return c;
   }
 
   /**
    *
-   * @param a
-   * @param b
    * @returns Returns the integer division of two unsigned integers. Reverts on
    * division by zero. The result is rounded towards zero.
    */
   static div(a: u64, b: u64): u64 {
-      assert(b > 0, "SafeMath: division by zero");
-      const c = a / b;
+    assert(b > 0, 'SafeMath: division by zero');
+    const c = a / b;
 
-      return c;
+    return c;
   }
 }
