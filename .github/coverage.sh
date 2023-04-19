b14f61e1416e720ce35211b38aa858e99eac3143
Ben <br@massa.net>
2023-04-19 14:58:07 +0200

    [coverage] test

--- .github/coverage.sh
+++ .github/coverage.sh
@@ -27,16 +27,6 @@ if [ "$oldCoverage" != "$coverage" ] || [ -z "$oldCoverage" ]; then
     newLine="![check-code-coverage](https://img.shields.io/badge/coverage-$coverage%25-$color)"
 
     updated_file=$(sed "2s/.*/$new_line/" $filename)
-
-    encodedContent=$(echo "$updated_file" | base64 --wrap=0)
-
-    response=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
-    https://api.github.com/repos/$GITHUB_REPOSITORY/contents/README.md?ref=$GITHUB_REF)
-
-    sha=$(echo "$response" | jq -r '.sha')
-    url=$(echo "$response" | jq -r '.url')
-
-    curl -s -H "Authorization: token $GITHUB_TOKEN" \
-    -X PUT -d "{\"message\":\"Update README\",\"content\":\"$encodedContent\",\"sha\":\"$sha\",\"branch\":\"$GITHUB_REF\"}" \
-    $url
+    echo "$updated_file"
+    echo "$updated_file" > $filename
 fi
