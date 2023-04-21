73df01924859fd138d08883cbf35a55fce1d8c37
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-04-21 15:01:59 +0200

    Update packages/as-types/assembly/result.ts
    Co-authored-by: Benjamin Reynes <44082144+Ben-Rey@users.noreply.github.com>

--- packages/as-types/assembly/result.ts
+++ packages/as-types/assembly/result.ts
@@ -39,7 +39,7 @@ export class Result<T> {
   /**
    * Returns the value if the `Result` is successful. Throws an assertion error if not.
    *
-   * @param msg - A string representing the message to be shown on error.
+   * @param msg - The message to be displayed if the `Result` has failed.
    * @returns
    * - Returns the value if the Result is okay.
    * - If the Result is not okay, it throws an assertion error with the given message.
