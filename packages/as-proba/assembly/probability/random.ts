c9b62829deeaa8e43ea3e950a9ffcae8b5e25368
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-04-21 11:09:29 +0200

    Update random.ts documentation

--- packages/as-proba/assembly/probability/random.ts
+++ packages/as-proba/assembly/probability/random.ts
@@ -1,10 +1,14 @@
 /**
- * Returns a random number between given limits.
+ * Returns a non random secure number between given limits.
  *
- * Lower and upper limits are possible values.
+ * @remarks
+ * - Lower and upper limits are possible values.
+ * - This function is not cryptographically secure.
  *
  * @param ll - included lower limit.
  * @param ul - included upper limit.
+ * @returns a non random secure number between given limits.
+ *
  */
 export function randomInt(ll: u64, ul: u64): u64 {
   return ll + u64(Math.round(f64(ul - ll) * Math.random()));
