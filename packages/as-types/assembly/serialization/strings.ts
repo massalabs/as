ad65ed9ea1e8776b85ed79ecaaf897000327c58a
0xSwapFeeder <tristan.gremont@epitech.eu>
2023-04-21 15:10:48 +0200

    docs(as-types-strings): added remarks

--- packages/as-types/assembly/serialization/strings.ts
+++ packages/as-types/assembly/serialization/strings.ts
@@ -1,7 +1,12 @@
 /**
  * Converts utf-16 string to a StaticArray<u8>.
  *
+ * @remarks
+ * If the string is empty, returns an empty array.
+ *
  * @param str - the string to convert
+ *
+ * @returns the converted StaticArray<u8>
  */
 export function stringToBytes(str: string): StaticArray<u8> {
   if (!str.length) {
@@ -13,7 +18,12 @@ export function stringToBytes(str: string): StaticArray<u8> {
 /**
  * Converts StaticArray<u8> to a string.
  *
+ * @remarks
+ * If the array is empty, returns an empty string.
+ *
  * @param arr - the array to convert
+ *
+ * @returns the converted string
  */
 export function bytesToString(arr: StaticArray<u8>): string {
   if (!arr.length) {
