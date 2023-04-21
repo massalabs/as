06d8ead5e59a6e714d8b8d4ff73801cb46c93b17
Benjamin Reynes <44082144+Ben-Rey@users.noreply.github.com>
2023-04-21 11:58:02 +0200

    Update packages/as-types/assembly/serialization/staticArrays.ts
    Co-authored-by: Grégory Libert <gl@massa.net>

--- packages/as-types/assembly/serialization/staticArrays.ts
+++ packages/as-types/assembly/serialization/staticArrays.ts
@@ -2,7 +2,7 @@
  * Converts a `StaticArray<u8>` to an `Uint8Array`.
  *
  * @param arr - The `StaticArray<u8>` to be converted.
- * @returns A `Uint8Array` representation of the input `StaticArray<u8>`.
+ * @returns An `Uint8Array` representation of the input `StaticArray<u8>`.
  */
 // @ts-ignore: decorator
 @inline
