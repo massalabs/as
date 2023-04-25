108ad267e22335df9a8758851021a35ea1b919f2
Grégory Libert <gl@massa.net>
2023-04-25 17:45:14 +0200

    adding empty returned value

--- packages/as-transformer/src/helpers/protobuf.ts
+++ packages/as-transformer/src/helpers/protobuf.ts
@@ -42,7 +42,7 @@ message ${name} {
 ${fields}
 }`;
 
-  if (returnedType != 'void' && returnedType != 'null') {
+  if (returnedType && returnedType != 'void' && returnedType != 'null') {
     const argumentResponse: Argument = {
       name: 'value',
       type: returnedType,
