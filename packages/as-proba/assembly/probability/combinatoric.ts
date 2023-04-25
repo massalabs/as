b0dfeb42cd2e4e2a5a0d71e053ccab3402ad6d66
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-04-25 14:28:02 +0200

    Update combinatoric.ts (#161)
    Co-authored-by: 0xSwapFeeder <72012341+0xSwapFeeder@users.noreply.github.com>
    Co-authored-by: Benjamin Reynes <44082144+Ben-Rey@users.noreply.github.com>

--- packages/as-proba/assembly/probability/combinatoric.ts
+++ packages/as-proba/assembly/probability/combinatoric.ts
@@ -1,29 +1,54 @@
 /**
- * Returns the number of unordered subsets of k elements
- * taken from a set of n elements.
- *
+ * This method calculates the number of unordered subsets of k elements taken from a set of n elements, 
+ * also known as 'n choose k'. 
+ * 
+ * @remarks
+ * - The method uses the formula C(n,k) = n!/(k!(n-k)!) to calculate the number of combinations.
+ * - Requires n \> k \>= 0 to work properly. Otherwise, if k \>= n, the method returns the value of n!/k! 
+ * 
  * @param n - Number of elements in set.
  * @param k - Number of elements in subset.
+
+ * @returns 
+ * - If n \> k \>= 0 : the number of unordered subsets of k elements taken from a set of n elements.
+ * - If k \>= n : the value of n!/k!
+ * 
  */
 export function combination(n: u64, k: u64): f64 {
   return partialPermutation(n, k) / factorial(k);
 }
 
 /**
- * Returns the number of ordered subsets of k elements
- * taken from a set of n elements.
+ * This method calculates the number of ordered subsets of k elements taken from a set of n elements.
  *
+ * @remarks
+ * - The method uses the formula P(n,k) = n!/(n-k)! to calculate the number of permutations.
+ * - Requires n \> k \>= 0 to work properly. If k \> n, the method returns the value of n!.
+ * 
  * @param n - Number of elements in set.
  * @param k - Number of elements in subset.
+
+ * @returns 
+ * - The number of ordered subsets of k elements taken from a set of n elements.
+ * - If k \> n : the value of n!.
+ * 
  */
 export function partialPermutation(n: u64, k: u64): f64 {
   return factorial(n) / factorial(n - k);
 }
 
 /**
- * Returns the number of permutation of n elements.
- *
+ * 
+ * This method calculates the factorial of a non-negative integer n.
+ * ie the number of permutation of n elements.
+ * 
+ * @remarks
+ * - The function uses a loop to multiply all the integers from 2 to n inclusive to obtain the factorial.
+ * 
  * @param n - Number of elements.
+
+ * @returns The factorial of n.
+ * 
  */
 export function factorial(n: u64): f64 {
   let r: f64 = 1;
