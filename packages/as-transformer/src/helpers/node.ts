c03d8de64967a2b3db52dc8ef0afb416eeb6ac8d
Grégory Libert <gl@massa.net>
2023-04-27 22:11:34 +0200

    adds get dependencies and parse file helpers

--- packages/as-transformer/src/helpers/node.ts
+++ packages/as-transformer/src/helpers/node.ts
@@ -1,7 +1,10 @@
 import {
   FunctionDeclaration,
   IdentifierExpression,
+  Parser,
+  Source,
 } from 'assemblyscript/dist/assemblyscript.js';
+import { readFileSync } from 'fs';
 
 // inspired from https://github.com/as-pect/visitor-as/blob/master/src/utils.ts#L35
 // TODO: check if it's still needed.
@@ -16,4 +19,28 @@ export function hasDecorator(node: FunctionDeclaration, name: string): bool {
       (node) => (<IdentifierExpression>node.name).text === name,
     ) == true
   );
+}
+
+export function parseFile(filePath: string, parser: Parser, subDir: string): Source {
+  const nodeModulesIndex = filePath.indexOf('node_modules/');
+  let newParser = new Parser(parser.diagnostics);
+
+  if (nodeModulesIndex > -1) {
+    const shortFilePath = filePath.replace(/.*node_modules/i,'~lib');
+    newParser.parseFile(
+      readFileSync(filePath, 'utf-8'),
+      shortFilePath,
+      false
+    );
+  } else {
+    const shortFilePath = filePath.replace(process.cwd() + "/", "")
+    console.log(shortFilePath.replace("build/", subDir));
+    newParser.parseFile(
+      readFileSync(shortFilePath, 'utf-8'),
+      shortFilePath.replace("build/", subDir),
+      false
+    );
+  }
+
+  return newParser.sources.pop()!;
 }
---
+++ packages/as-transformer/src/helpers/typescript.ts
@@ -0,0 +1,56 @@
+import { EnumDeclaration, Project, SourceFile } from 'ts-morph';
+
+export function getDependencies(file: string): string[] {
+
+    const project = new Project();
+    project.addSourceFileAtPath(file);
+    
+    const dependencies = new Set<string>();
+    
+    getAllDependencies(project.getSourceFiles(), dependencies);
+
+    const subpath = file[0] == "." ? file.substring(1): file;
+
+    console.log(subpath);
+
+    console.log(Array.from(dependencies));
+    
+    return Array.from(dependencies).filter(d => d.indexOf(subpath) < 0);
+}
+
+function getAllDependencies(sourceFiles: SourceFile[], dependencies: Set<string>): void {
+    for (const sourceFile of sourceFiles) {
+
+      // Load dependencies from import declarations
+      const importDeclarations = sourceFile.getImportDeclarations();
+      const importedSources = importDeclarations
+        .map(imp => imp.getModuleSpecifierSourceFile())
+        .filter(src => src != undefined) as SourceFile[];
+  
+      // Load dependencies from exported declarations
+      const exportedDeclarationsArray = Array.from(
+        sourceFile.getExportedDeclarations().values()
+      ).flat() as Array<EnumDeclaration | undefined>;
+  
+      const exportedSourceFiles = exportedDeclarationsArray
+        .filter(
+          exportedDeclaration =>
+            exportedDeclaration &&
+            typeof exportedDeclaration.getSourceFile === 'function'
+        )
+        .map(exportedDeclaration => exportedDeclaration!.getSourceFile());
+  
+        const sources = [
+          ...importedSources,
+          ...exportedSourceFiles,
+        ] 
+  
+      for (const source of sources) {
+        const importedFilePath = source.getFilePath();
+        if (!dependencies.has(importedFilePath)) {
+          dependencies.add(importedFilePath);
+          getAllDependencies([source], dependencies);
+        }
+      }
+    }
+  }
