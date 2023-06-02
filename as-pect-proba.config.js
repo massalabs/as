2e44a64a4a6b207fb565151ee6529168bd12bb93
lilyanB <l.bastien@massa.net>
2023-06-02 14:12:09 +0200

    add as-pect-proba config

---
+++ as-pect-proba.config.js
@@ -0,0 +1,5 @@
+import config from './as-pect.config.js';
+config.entries = ['packages/as-proba/assembly/**/*.spec.ts'];
+config.include = ['packages/as-proba/assembly/**/*.include.ts'];
+config.disclude = [/node_modules/];
+export default config;
