aea0efa3aace9145e00da9be9d7dca6125d5939d
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-07-26 11:21:41 +0200

    add uint8ArrayToBase64 to as-types

---
+++ packages/as-types/assembly/uint8ArrayToBase64.ts
@@ -0,0 +1,37 @@
+/**
+ * Converts a Uint8Array to a base64 string.
+ *
+ * @param bytes - The Uint8Array to convert.
+ *
+ * @returns The base64 string.
+ */
+export function uint8ArrayToBase64(bytes: Uint8Array): string {
+  const base64Chars =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+  let result = '';
+  let padding = '';
+  let buffer = 0;
+  let bitsRemaining = 8;
+
+  for (let i = 0; i < bytes.length; i++) {
+    buffer = (buffer << 8) | bytes[i];
+    bitsRemaining += 8;
+
+    while (bitsRemaining >= 6) {
+      const index = (buffer >> (bitsRemaining - 6)) & 0x3f;
+      result += base64Chars.charAt(index);
+      bitsRemaining -= 6;
+    }
+  }
+
+  // If there are any remaining bits, pad and add padding characters ('=')
+  if (bitsRemaining > 0) {
+    buffer <<= 6 - bitsRemaining;
+    const index = buffer & 0x3f;
+    result += base64Chars.charAt(index);
+    padding = bitsRemaining === 2 ? '==' : bitsRemaining === 4 ? '=' : '';
+  }
+
+  return result + padding;
+}
