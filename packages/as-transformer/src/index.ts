80cc1c9bd8d5ca3b24cc0ecf2b23b057cce28d97
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-08-10 16:55:36 +0200

    Update index.ts

--- packages/as-transformer/src/index.ts
+++ packages/as-transformer/src/index.ts
@@ -2,5 +2,6 @@ import { Transformer } from './transformer.js';
 export default Transformer;
 export {
   MassaCustomType,
+  extractTypes,
   fetchCustomTypes,
 } from './helpers/customTypeParser.js';
