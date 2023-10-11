befb32a54b341847fd05f99d068897aa1901a02a
Ben <br@massa.net>
2023-10-11 12:53:45 +0200

    push

--- scripts/add_versions.sh
+++ scripts/add_versions.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -ex
 
-for packageDir in packages/*; do
-    if [ -d "$packageDir" ]; then
-        PACKAGE_NAME=$(cat "$packageDir/package.json" | jq -r '.name')
-        PUBLISH_VERSION=$(cat "$packageDir/package.json" | jq -r '.version')
+# for packageDir in packages/*; do
+#     if [ -d "$packageDir" ]; then
+#         PACKAGE_NAME=$(cat "$packageDir/package.json" | jq -r '.name')
+#         PUBLISH_VERSION=$(cat "$packageDir/package.json" | jq -r '.version')
 
-        npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
-        npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" testnet
-    fi
-done
+#         npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" buildnet
+#         npm dist-tag add "$PACKAGE_NAME@$PUBLISH_VERSION" testnet
+#     fi
+# done
+
+cd ./packages/as-types
+pwd
+npm publish --access public --tag latest
+npm dist-tag add "@massalabs/as-types@2.0.0" buildnet
