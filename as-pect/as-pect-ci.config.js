586b559e49dccbdf4b28e6b0a1f782b9af129291
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-11-08 16:09:27 +0100

    Update dependencies

--- as-pect/as-pect-ci.config.js
+++ as-pect/as-pect-ci.config.js
@@ -1,5 +1,6 @@
 import config from '../as-pect.config.js';
 // coverage option makes the compilation fail :(
+// Should be fixed in: https://github.com/as-pect/visitor-as/pull/44 and https://github.com/as-pect/as-covers/pull/13
 // the issue seems fixed when using latest as-pect version (and latest assemblyscript version)
-// config.coverage = ['packages/**/assembly/**/*.ts'];
+config.coverage = ['packages/**/assembly/**/*.ts'];
 export default config;
