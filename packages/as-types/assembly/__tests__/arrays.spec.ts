784ff1fbcec8b105d8ecd4ae453c9b1d36e5ae87
BenRey <44082144+Ben-Rey@users.noreply.github.com>
2024-06-06 16:04:33 +0200

    Update packages/as-types/assembly/__tests__/arrays.spec.ts
    Co-authored-by: Grégory Libert <gl@massa.net>

--- packages/as-types/assembly/__tests__/arrays.spec.ts
+++ packages/as-types/assembly/__tests__/arrays.spec.ts
@@ -3,7 +3,7 @@ import { staticArrayToUint8Array } from '../serialization';
 describe('arrays', () => {
   test('staticArrayToUint8Array', () => {
     const staticArray = StaticArray.fromArray<u8>([0, 1, 2, 3, 4]);
-    const result = staticArrayToUint8Array(myArray);
+    const typedArray = staticArrayToUint8Array(myArray);
 
     expect(result instanceof Uint8Array).toBe(true);
     expect(result.length).toBe(myArray.length);
